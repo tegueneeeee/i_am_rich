1006de412f60bdde8ccf970a11de4a0cd97dff1d
Kim Taekwon <kimxordnjs@naver.com>
2022-05-18 15:48:49 +0900

    [ADD]I Am Rich App

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage(
+              'images/diamond.png',
+            ),
+          ),
+        ),
       ),
     ),
   );
